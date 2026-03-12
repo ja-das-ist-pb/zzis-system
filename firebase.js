7f0a1892832483e4541a218428b98bcf31cdec3a
js-das-ist-pb <paulpb0725@gmail.com>
2026-03-12 23:40:44 +0800

    first commit

---
+++ firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCN9sLWsAT6tSJ59BKlD2fM011iDcwAdnQ",
+  authDomain: "zzis-system.firebaseapp.com",
+  projectId: "zzis-system",
+  storageBucket: "zzis-system.firebasestorage.app",
+  messagingSenderId: "755538927348",
+  appId: "1:755538927348:web:b63c0a791c96f52902c8a1",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
